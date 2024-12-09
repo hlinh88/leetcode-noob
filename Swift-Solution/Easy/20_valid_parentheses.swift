ef8d398f225a1bb0f50cac31c2a9583df8ffb9e2
Luke Nguyen <luke@outcubator.com>
2024-12-09 16:58:25 +0700

    Valid parentheses

---
+++ Swift-Solution/Easy/20_valid_parentheses.swift
@@ -0,0 +1,28 @@
+//Input: s = "()[]{}"
+//
+//Output: true
+
+class Solution {
+    func isValid(_ s: String) -> Bool {
+
+        guard s.count % 2 == 0 else { return false }
+
+        var stack: [Character] = []
+
+        for ch in s {
+            switch ch {
+            case "(": stack.append(")")
+            case "[": stack.append("]")
+            case "{": stack.append("}")
+            default:
+                if stack.isEmpty || stack.removeLast() != ch {
+                    return false
+                }
+            }
+        }
+
+        return stack.isEmpty
+    }
+}
+
+Solution().isValid("()[]{}")
