dc7d196d4cdc1388a12f542c86f67218fc3616c2
Luke Nguyen <luke@outcubator.com>
2025-02-10 18:24:15 +0700

    3105. Longest Strictly Increasing or Strictly Decreasing Subarray

--- Swift-Solution/Easy/3105_longest_strictly_increase_decrease_subarray.swift
+++ Swift-Solution/Easy/3105_longest_strictly_increase_decrease_subarray.swift
@@ -1,12 +1,23 @@
-import Foundation
-
+//Example 1:
+//
+//Input: nums = [1,4,3,3,2]
+//
+//Output: 2
+//
+//Explanation:
+//
+//The strictly increasing subarrays of nums are [1], [2], [3], [3], [4], and [1,4].
+//
+//The strictly decreasing subarrays of nums are [1], [2], [3], [3], [4], [3,2], and [4,3].
+//
+//Hence, we return 2.
 
 class Solution {
     func longestMonotonicSubarray(_ nums: [Int]) -> Int {
         var res = 1
         var count = 1
         var state = 0
-        
+
         for i in 0..<nums.count-1 {
             if nums[i] < nums[i+1] {
                 if state == 1 {
@@ -16,7 +27,7 @@ class Solution {
                     state = 1
                 }
             }
-            
+
             if nums[i] > nums[i+1] {
                 if state == -1 {
                     count += 1
@@ -25,16 +36,16 @@ class Solution {
                     state = -1
                 }
             }
-            
+
             if nums[i] == nums[i+1] {
                 state = 0
                 count = 1
             }
-            
+
             res = max(res, count)
             print("count: \(count), res: \(res), state: \(state)")
         }
-        
+
         return res
     }
 }
