b844a2abb9429180cd3528ee1c3cd8629161604e
Luke Nguyen <luke@outcubator.com>
2025-02-11 15:20:54 +0700

    55 Jump Game

--- Swift-Solution/Medium/55_jump_game.swift
+++ Swift-Solution/Medium/55_jump_game.swift
@@ -1,18 +1,26 @@
-import Foundation
-
+//Example 1:
+//
+//Input: nums = [2,3,1,1,4]
+//Output: true
+//Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+//Example 2:
+//
+//Input: nums = [3,2,1,0,4]
+//Output: false
+//Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
 
 class Solution {
     func canJump(_ nums: [Int]) -> Bool {
         var goal = nums.count - 1
-        
+
         for i in stride(from: nums.count - 1, through: 0, by: -1) {
             if i + nums[i] >= goal {
                 goal = i
             }
         }
-        
+
         return goal == 0
-        
+
 //        var maxReach = 0
 //        for i in 0...nums.count-1 {
 //            if i > maxReach {
