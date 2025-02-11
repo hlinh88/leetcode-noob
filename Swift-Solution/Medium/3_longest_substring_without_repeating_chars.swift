dab9871629cfda7f696bb048e7d6ad9c03a46206
Luke Nguyen <luke@outcubator.com>
2025-02-11 12:31:28 +0700

    3 Longest Substring Without Repeating Characters

--- Swift-Solution/Medium/3_longest_substring_without_repeating_chars.swift
+++ Swift-Solution/Medium/3_longest_substring_without_repeating_chars.swift
@@ -1,12 +1,14 @@
-import Foundation
-
+//Input: s = "pwwkew"
+//Output: 3
+//Explanation: The answer is "wke", with the length of 3.
+//Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
 class Solution {
     func lengthOfLongestSubstring(_ s: String) -> Int {
 //        var res = 0
 //        var s = Array(s)
 //        var set: Set<Character> = []
-//        
+//
 //        for i in 0..<s.count {
 //            for j in i..<s.count {
 //                if !set.contains(s[j]) {
@@ -18,27 +20,27 @@ class Solution {
 //                }
 //            }
 //        }
-//        
+//
 //        print(res)
 //        return res
-        
+
         let characters = Array(s)
         var charIndexMap = [Character: Int]()
         var maxLength = 0
         var start = 0
-        
+
         for end in 0..<characters.count {
             let currentChar = characters[end]
-            
+
             if let previousIndex = charIndexMap[currentChar] {
                 start = max(start, previousIndex + 1)
             }
-            
+
             charIndexMap[currentChar] = end
             maxLength = max(maxLength, end - start + 1)
             print("charIndexMap: \(charIndexMap), maxLength: \(maxLength), start: \(start)")
         }
-        
+
         return maxLength
     }
 }
