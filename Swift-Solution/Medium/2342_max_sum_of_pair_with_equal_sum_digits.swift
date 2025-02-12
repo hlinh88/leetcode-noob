20827bb181d3c30abbd5d68137ba8c604a0d422a
Luke Nguyen <luke@outcubator.com>
2025-02-12 10:31:52 +0700

    2342. Max Sum of a Pair With Equal Sum of Digits

--- Swift-Solution/Medium/2342_max_sum_of_pair_with_equal_sum_digits.swift
+++ Swift-Solution/Medium/2342_max_sum_of_pair_with_equal_sum_digits.swift
@@ -1,11 +1,15 @@
-import Foundation
-
+//Input: nums = [18,43,36,13,7]
+//Output: 54
+//Explanation: The pairs (i, j) that satisfy the conditions are:
+//- (0, 2), both numbers have a sum of digits equal to 9, and their sum is 18 + 36 = 54.
+//- (1, 4), both numbers have a sum of digits equal to 7, and their sum is 43 + 7 = 50.
+//So the maximum sum that we can obtain is 54.
 
 class Solution {
     func maximumSum(_ nums: [Int]) -> Int {
         var res = -1
         var dict: [Int: [Int]] = [:]
-        
+
         for num in nums {
             var calNum = num
             var total = 0
@@ -13,10 +17,10 @@ class Solution {
                 total += calNum % 10
                 calNum /= 10
             }
-            
+
             dict[total, default: []].append(num)
         }
-        
+
         for value in dict.values {
             var value = value
             if value.count > 1 {
@@ -24,7 +28,7 @@ class Solution {
                 res = max(res, value[value.count - 1] + value[value.count - 2])
             }
         }
-        
+
         return res
     }
 }
