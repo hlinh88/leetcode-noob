7d6182b0369b0404a2ee1325073098242a7c6aeb
Luke Nguyen <luke@outcubator.com>
2025-02-14 17:56:16 +0700

    - 125 valid palindrome

--- Swift-Solution/Easy/125_valid_palindrome.swift
+++ Swift-Solution/Easy/125_valid_palindrome.swift
@@ -1,17 +1,19 @@
-import Foundation
+//Input: s = "A man, a plan, a canal: Panama"
+//Output: true
+//Explanation: "amanaplanacanalpanama" is a palindrome.
 
 class Solution {
     func isPalindrome(_ s: String) -> Bool {
         let s = Array(s.lowercased().filter { $0.isLetter || $0.isNumber })
         var left = 0
         var right = s.count - 1
-        
+
         while left <= right {
             if s[left] != s[right] { return false }
             left += 1
             right -= 1
         }
-        
+
         return true
     }
 }
