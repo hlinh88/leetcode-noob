bfa27a02e7ef607faeb9fc94f480d54bc0f9b48b
Luke Nguyen <hlinh.jobs@gmail.com>
2025-02-23 13:34:54 +0700

    Daily 23/2 - 889 binary tree preorder postorder

--- Medium/889_construct_binary_tree_preorder_postorder_traversal.swift
+++ Medium/889_construct_binary_tree_preorder_postorder_traversal.swift
@@ -1,3 +1,15 @@
+// Given two integer arrays, preorder and postorder where preorder is the preorder traversal of a binary tree of distinct values and postorder is the postorder traversal of the same tree, reconstruct and return the binary tree.
+
+// If there exist multiple answers, you can return any of them.
+
+ 
+
+// Example 1:
+
+
+// Input: preorder = [1,2,4,5,3,6,7], postorder = [4,5,2,6,7,3,1]
+// Output: [1,2,3,4,5,6,7]
+
 /**
  * Definition for a binary tree node.
  */
