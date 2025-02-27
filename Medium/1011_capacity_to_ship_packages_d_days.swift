4012a830c805fb6d6d9916ddf3695102b0a6982f
Luke Nguyen <luke@outcubator.com>
2025-02-27 18:17:26 +0700

    - 1011 capacity to ship packages d days

--- Medium/1011_capacity_to_ship_packages_d_days.swift
+++ Medium/1011_capacity_to_ship_packages_d_days.swift
@@ -1,8 +1,21 @@
+//Input: weights = [1,2,3,4,5,6,7,8,9,10], days = 5
+//Output: 15
+//Explanation: A ship capacity of 15 is the minimum to ship all the packages in 5 days like this:
+//1st day: 1, 2, 3, 4, 5
+//2nd day: 6, 7
+//3rd day: 8
+//4th day: 9
+//5th day: 10
+//
+//Note that the cargo must be shipped in the order given, so using a ship of capacity 14 and splitting the packages into parts like (2, 3, 4, 5), (1, 6, 7), (8), (9), (10) is not allowed.
+
+// Binary Search
+
 class Solution {
     func shipWithinDays(_ weights: [Int], _ days: Int) -> Int {
         var l = weights.max()!
         var r = weights.reduce(0) { $0 + $1 }
-        
+
         func isValid(_ capacity: Int, _ days: Int) -> Bool {
             var dayNeeds = 1
             var sum = 0
@@ -17,7 +30,7 @@ class Solution {
             }
             return true
         }
-        
+
         while l <= r {
             var mid = l + (r - l) / 2
             print("L: \(l), R: \(r), M: \(mid)")
@@ -27,11 +40,11 @@ class Solution {
                 l = mid + 1
             }
         }
-        
+
         return l
     }
-    
-  
+
+
 }
 
 Solution().shipWithinDays([1,2,3,1,1], 4)
