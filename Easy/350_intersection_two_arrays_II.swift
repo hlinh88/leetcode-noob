97c6098951da6ce0ba418ef2a34a68ecdbf15c1e
Luke Nguyen <luke@outcubator.com>
2025-02-28 18:15:09 +0700

    -350 intersection two arrays II

--- Easy/350_intersection_two_arrays_II.swift
+++ Easy/350_intersection_two_arrays_II.swift
@@ -1,12 +1,17 @@
+//Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+//Output: [4,9]
+//Explanation: [9,4] is also accepted.
+
+
 class Solution {
     func intersect(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
         var res: [Int] = []
         var dict: [Int: Int] = [:]
-        
+
         for num in nums1 {
             dict[num, default: 0] += 1
         }
-        
+
         for num in nums2 {
             if let value = dict[num] {
                 res.append(num)
@@ -16,9 +21,7 @@ class Solution {
                 }
             }
         }
-        
+
         return res
     }
 }
-
-Solution().intersect([1,2,2,1], [2,2])
