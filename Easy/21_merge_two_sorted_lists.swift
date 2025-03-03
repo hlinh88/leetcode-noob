d8d0288f3fa21255dded10094f2e4221bf12d733
Luke Nguyen <luke@outcubator.com>
2025-03-03 11:57:45 +0700

    Linked List - 21 merge two sorted list - 141 linked list cycle - 876 middle of linked list

--- Easy/21_merge_two_sorted_lists.swift
+++ Easy/21_merge_two_sorted_lists.swift
@@ -16,13 +16,13 @@ public class ListNode {
     public init(_ val: Int) { self.val = val; self.next = nil; }
     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
 }
- 
+
 class Solution {
     func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
         var merged = ListNode(0)
         var cur = merged
         var l1 = list1, l2 = list2
-        
+
         while l1 != nil && l2 != nil {
             if l1!.val < l2!.val {
                 cur.next = l1
@@ -33,13 +33,13 @@ class Solution {
             }
             cur = cur.next!
         }
-        
+
         if l1 != nil {
             cur.next = l1
         } else {
             cur.next = l2
         }
-        
+
         return merged.next
     }
 }
