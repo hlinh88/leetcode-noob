37655f62fa9c72bdaca53713ccbc3bc4604445e4
Luke Nguyen <luke@outcubator.com>
2025-03-24 10:25:44 +0700

    Dailly 24/3 - Merge intervals

--- Medium/3169 - Count Days Without Meetings.swift
+++ Medium/3169 - Count Days Without Meetings.swift
@@ -1,3 +1,13 @@
+// Input: days = 10, meetings = [[5,7],[1,3],[9,10]]
+
+// Output: 2
+
+// Explanation:
+
+// There is no meeting scheduled on the 4th and 8th days.
+
+// Sort -> Merge intervals
+
 class Solution {
     func countDays(_ days: Int, _ meetings: [[Int]]) -> Int {
         var meetings = meetings.sorted { $0[0] < $1[0] }
@@ -17,5 +27,3 @@ class Solution {
         return freeDays
     }
 }
-
-Solution().countDays(10, [[5,7],[1,3],[9,10]])
