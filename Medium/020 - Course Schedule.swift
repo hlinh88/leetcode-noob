9a4aa435ba2a0401810a5a072119c3a08acb3cfc
Luke Nguyen <hlinh.jobs@gmail.com>
2025-03-27 16:57:06 +0700

    - 207 course schedule

--- Medium/020 - Course Schedule.swift
+++ Medium/020 - Course Schedule.swift
@@ -1,3 +1,12 @@
+// There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+// For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+// Return true if you can finish all courses. Otherwise, return false.
+
+// DFS
+// Adj list
+// Node -> All prereq -> If true []
+
 class Solution {
     func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
         var adjList: [[Int]] = Array(repeating: [], count: numCourses)
