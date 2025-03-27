17535baf97ff4dc89120c7d19d51398201ac8efd
Luke Nguyen <hlinh.jobs@gmail.com>
2025-03-27 16:24:47 +0700

    Daily 27/3

--- Medium/2780 - Minimum Index of a Valid Split.swift
+++ Medium/2780 - Minimum Index of a Valid Split.swift
@@ -1,3 +1,11 @@
+// Input: nums = [2,1,3,1,1,1,7,1,2,1]
+// Output: 4
+// Explanation: We can split the array at index 4 to obtain arrays [2,1,3,1,1] and [1,7,1,2,1].
+// In array [2,1,3,1,1], element 1 is dominant since it occurs thrice in the array and 3 * 2 > 5.
+// In array [1,7,1,2,1], element 1 is dominant since it occurs thrice in the array and 3 * 2 > 5.
+// Both [2,1,3,1,1] and [1,7,1,2,1] have the same dominant element as nums, so this is a valid split.
+// It can be shown that index 4 is the minimum index of a valid split.
+
 class Solution {
     func minimumIndex(_ nums: [Int]) -> Int {
         var res = -1
