f05b5354c20213d400ffd298fcce727991ca2ff7
Luke Nguyen <luke@outcubator.com>
2025-03-31 10:44:54 +0700

    Daily 30/3 - Sapa trip

--- Medium/0763 - Partition Labels.swift
+++ Medium/0763 - Partition Labels.swift
@@ -1,3 +1,10 @@
+// Input: s = "ababcbacadefegdehijhklij"
+// Output: [9,7,8]
+// Explanation:
+// The partition is "ababcbaca", "defegde", "hijhklij".
+// This is a partition so that each letter appears in at most one part.
+// A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+
 class Solution {
     func partitionLabels(_ s: String) -> [Int] {
         var lastIndex: [Character: Int] = [:]
@@ -6,7 +13,6 @@ class Solution {
             lastIndex[c, default: i] = i
         }
         
-        print(lastIndex)
         var result: [Int] = []
         var start = 0
         var end = 0
@@ -22,5 +28,3 @@ class Solution {
         return result
     }
 }
-
-Solution().partitionLabels("ababcbacadefegdehijhklij")
