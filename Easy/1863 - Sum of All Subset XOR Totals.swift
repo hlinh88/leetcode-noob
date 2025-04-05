7f900a81ca04d951264888915a621cdf31b4ddef
Hoang Linh <hlinh.jobs@gmail.com>
2025-04-05 14:01:42 +0700

    Update 1863 - Sum of All Subset XOR Totals.swift
    a linh pro backtracking

--- Easy/1863 - Sum of All Subset XOR Totals.swift
+++ Easy/1863 - Sum of All Subset XOR Totals.swift
@@ -2,16 +2,25 @@ class Solution {
     func subsetXORSum(_ nums: [Int]) -> Int {
         var res = 0 
 
+        // For loop solution PRo vip
         func dfs(_ index: Int, _ xor: Int) {
-            if index == nums.count {
-                res += xor
-                return
-            }
+            res += xor
 
-            // Skip or take
-            dfs(index + 1, xor ^ nums[index])
-            dfs(index + 1, xor)
+            for i in index..<nums.count {
+                dfs(i+1, xor ^ nums[i])
+            }
         }
+
+        // Skip or take 
+        // func dfs(_ index: Int, _ xor: Int) {
+        //     if index == nums.count {
+        //         res += xor
+        //         return
+        //     }
+        
+        //     dfs(index + 1, xor ^ nums[index])
+        //     dfs(index + 1, xor)
+        // }
 
         dfs(0, 0)
 
