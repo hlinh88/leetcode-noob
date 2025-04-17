5c51e4cf8425531025eeca00e34e9691d4aca9fd
Luke Nguyen <luke@outcubator.com>
2025-04-17 10:23:25 +0700

    Daily 17/4

--- Easy/2176 - Count Equal and Divisible Pairs in an Array.swift
+++ Easy/2176 - Count Equal and Divisible Pairs in an Array.swift
@@ -1,15 +1,8 @@
 class Solution {
     func countPairs(_ nums: [Int], _ k: Int) -> Int {
+        // Hash Map
         var count = 0
         var map: [Int: [Int]] = [:]
-//        for i in 0..<nums.count {
-//            for j in i+1..<nums.count {
-//                if nums[i] == nums[j] && (i * j) % k == 0 {
-//                    count += 1
-//                }
-//            }
-//        }
-        
         for (i, num) in nums.enumerated() {
             for j in map[num, default: []] {
                 if (i * j) % k == 0 {
@@ -19,6 +12,15 @@ class Solution {
             
             map[num, default: []].append(i)
         }
+
+        // Brute Force
+//        for i in 0..<nums.count {
+//            for j in i+1..<nums.count {
+//                if nums[i] == nums[j] && (i * j) % k == 0 {
+//                    count += 1
+//                }
+//            }
+//        }
         
         return count
     }
